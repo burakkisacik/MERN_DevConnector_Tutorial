10c74999d0bd92662708a9e4af8ef9d829592b9b
burak <burakkisacik@yahoo.com>
2021-03-26 11:39:18 +0300

    DB connection: mongoose connection established, server.js edited

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+//load env file in order to use environment variables
+dotenv.config({ path: './config/config.env' });
+
+//Connect the DataBase
+connectDB();
 
 const app = express();
 
