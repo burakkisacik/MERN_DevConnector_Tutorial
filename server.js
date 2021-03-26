a0c8f4d7ae32d0b186efab768b2ae65d87d18fb0
burak <burakkisacik@yahoo.com>
2021-03-26 11:47:35 +0300

    Routes created: routes folder holds the api routes and server.js edited

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.get('/', (req, res) => {
   res.send('initial setup');
 });
 
+//Define routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
